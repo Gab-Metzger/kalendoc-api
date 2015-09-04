058a4f7d4d7f09301e46edea241a10cebd1a6f53
Gab-Metzger <gab.metzger@gmail.com>
2015-09-04 12:08:15 +0200

    Remove mongo dev connection

--- config/connections.js
+++ config/connections.js
@@ -29,13 +29,6 @@ module.exports.connections = {
    * Run:
    * npm install sails-mongo
    */
-  mongoDevServer: {
-    adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    database: 'kalendoc'
-  },
-
   mongoProdServer: {
     adapter: 'sails-mongo',
     url: process.env.MONGO_URL
