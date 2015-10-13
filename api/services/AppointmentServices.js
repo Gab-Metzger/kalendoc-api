2aff19630bb36dc3b6b62948c311108ece186390
Gab-Metzger <gab.metzger@gmail.com>
2015-10-13 07:55:43 +0200

    logs res

--- api/services/AppointmentServices.js
+++ api/services/AppointmentServices.js
@@ -138,6 +138,7 @@ function findWeeklyAppointment(start, doctor, callback) {
             findWeeklyAppointment(currentDate.add(7, 'days').toISOString(), doctor, callback);
           } else {
             res = organizeAppointmentsByWeek(res, start);
+            console.log(res);
             return callback(res);
           }
         }
