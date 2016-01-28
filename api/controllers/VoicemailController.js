5aae33454c063a8014e05f793978a25ebd98368c
Gab-Metzger <gab.metzger@gmail.com>
2016-01-28 18:49:49 +0100

    Check if there is a voicemail before saving it

--- api/controllers/VoicemailController.js
+++ api/controllers/VoicemailController.js
@@ -8,21 +8,25 @@
 module.exports = {
 	create: function(req, res) {
     var params = req.params.all();
-    Doctor.findOne({aircallNumber: params.data.number.digits}).exec(function (err, doctor) {
-      if (err) {
-        console.log(err);
-        return res.json(404, {err: "No doctor found"});
-      } else {
-        Voicemail.create({doctor: doctor.id, url: params.data.voicemail}).exec(function (err, voicemail) {
-          if (err) {
-            console.log(err);
-            return res.json(500, {err: 'Error on voicemail creation'});
-          } else {
-            return res.json(200, voicemail);
-          }
-        });
-      }
-    });
+    if (params.data.voicemail) {
+      Doctor.findOne({aircallNumber: params.data.number.digits}).exec(function (err, doctor) {
+        if (err) {
+          console.log(err);
+          return res.json(404, {err: "No doctor found"});
+        } else {
+          Voicemail.create({doctor: doctor.id, url: params.data.voicemail}).exec(function (err, voicemail) {
+            if (err) {
+              console.log(err);
+              return res.json(500, {err: 'Error on voicemail creation'});
+            } else {
+              return res.json(200, voicemail);
+            }
+          });
+        }
+      });
+    } else {
+      return res.json(200, {message: "No voicemail, nothing to do."})
+    }
   }
 };
 
