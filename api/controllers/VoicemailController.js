71c6bf3075930a5d342e35b4317f65e2f39bb1a7
Gab-Metzger <gab.metzger@gmail.com>
2016-01-28 18:42:01 +0100

    Link aircall number to doctor and save voicemail in database

--- api/controllers/VoicemailController.js
+++ api/controllers/VoicemailController.js
@@ -8,25 +8,21 @@
 module.exports = {
 	create: function(req, res) {
     var params = req.params.all();
-    console.log("AIRCALL PARAMS");
-    console.log(params);
-    return res.json(200, params);
-    //TODO: Find link between aircall object and doctor
-    // Doctor.findOne({lastName: params.lastName}).exec(function (err, doctor) {
-    //   if (err) {
-    //     console.log(err);
-    //     return res.json(404, {err: "No doctor found"});
-    //   } else {
-    //     Voicemail.create({doctor: doctor.id, url: params.url}).exec(function (err, voicemail) {
-    //       if (err) {
-    //         console.log(err);
-    //         return res.json(500, {err: 'Error on voicemail creation'});
-    //       } else {
-    //         return res.json(200, voicemail);
-    //       }
-    //     });
-    //   }
-    // });
+    Doctor.findOne({aircallNumber: params.data.number.digits}).exec(function (err, doctor) {
+      if (err) {
+        console.log(err);
+        return res.json(404, {err: "No doctor found"});
+      } else {
+        Voicemail.create({doctor: doctor.id, url: params.data.voicemail}).exec(function (err, voicemail) {
+          if (err) {
+            console.log(err);
+            return res.json(500, {err: 'Error on voicemail creation'});
+          } else {
+            return res.json(200, voicemail);
+          }
+        });
+      }
+    });
   }
 };
 
