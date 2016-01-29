d7db467442d81867d313c39084195ed735c7bebe
Gab-Metzger <gab.metzger@gmail.com>
2016-01-29 09:37:22 +0100

    Add google web speech integration for voicemail

--- api/controllers/VoicemailController.js
+++ api/controllers/VoicemailController.js
@@ -5,6 +5,14 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+var request = require('superagent');
+var speech = require('google-speech-api');
+var opts = {
+  filtetype: 'mp3',
+  lang: 'fr',
+  key: process.env.SPEECH_KEY
+}
+
 module.exports = {
 	create: function(req, res) {
     var params = req.params.all();
@@ -14,14 +22,39 @@ module.exports = {
           console.log(err);
           return res.json(404, {err: "No doctor found"});
         } else {
-          Voicemail.create({doctor: doctor.id, url: params.data.voicemail}).exec(function (err, voicemail) {
+          request
+          .get(params.data.voicemail)
+          .pipe(speech(opts, function (err, results) {
             if (err) {
-              console.log(err);
-              return res.json(500, {err: 'Error on voicemail creation'});
+              Voicemail.create({doctor: doctor.id, url: params.data.voicemail}).exec(function (err, voicemail) {
+                if (err) {
+                  console.log(err);
+                  return res.json(500, {err: 'Error on voicemail creation'});
+                } else {
+                  return res.json(200, voicemail);
+                }
+              });
+            } else if (results[0]) {
+              var text = results[0].result[0].alternative[0].transcript;
+              Voicemail.create({doctor: doctor.id, url: params.data.voicemail, text: text}).exec(function (err, voicemail) {
+                if (err) {
+                  console.log(err);
+                  return res.json(500, {err: 'Error on voicemail creation'});
+                } else {
+                  return res.json(200, voicemail);
+                }
+              });
             } else {
-              return res.json(200, voicemail);
+              Voicemail.create({doctor: doctor.id, url: params.data.voicemail}).exec(function (err, voicemail) {
+                if (err) {
+                  console.log(err);
+                  return res.json(500, {err: 'Error on voicemail creation'});
+                } else {
+                  return res.json(200, voicemail);
+                }
+              });
             }
-          });
+          }));
         }
       });
     } else {
