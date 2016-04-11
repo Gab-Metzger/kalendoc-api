34c0202e4940d08725f058d4010f2a0c5c521dd0
Gab-Metzger <gab.metzger@gmail.com>
2016-04-11 20:02:14 +0200

    Add endpoint url to find patient by ID

--- config/policies.js
+++ config/policies.js
@@ -82,7 +82,7 @@ module.exports.policies = {
   },
   PatientController: {
     'create': true,
-    'find' : ['loggedIn', 'medicalPro'],
+    'find' : true,
     'findOne': ['loggedIn', 'medicalPro'],
     'findName' : ['loggedIn','medicalPro'],
     'findAppointments': ['loggedIn'],
